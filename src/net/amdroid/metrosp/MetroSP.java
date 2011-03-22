da455ee3b1a0692720607b1a08b6c18b64d5bc9b
Lucio Maciel <luciofm@amdroid.net>
2011-03-21 23:44:59 -0300

    Added Copyright note.
    This code is released under the GPLv2 License.
    
    Signed-off-by: Lucio Maciel <luciofm@amdroid.net>

--- src/net/amdroid/metrosp/MetroSP.java
+++ src/net/amdroid/metrosp/MetroSP.java
@@ -1,3 +1,28 @@
+/*
+ *
+ *  Metro SP
+ *
+ *  Copyright (C) 2011  Am.Droid (www.amdroid.net). All rights reserved.
+ *  Copyright (C) 2011  Lucio Maciel <luciofm@amdroid.net>. All rights 
+ *  reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+
+
 package net.amdroid.metrosp;
 
 import java.io.BufferedReader;
